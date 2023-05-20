eb6d47cc810f268ae963e8e2fffde7cf6e702c69
Scott Hallock <scott.hallock@gmail.com>
2023-05-19 20:47:57 -0500

    styles: logo

--- src/App.jsx
+++ src/App.jsx
@@ -30,6 +30,7 @@ import {
   SCENE_ACTION,
   LIGHT_ACTION,
 } from '../utils/types';
+import Logo from './components/Logo';
 
 function App() {
   const [scene, setScene] = useState(initialScene);
@@ -179,23 +180,20 @@ function App() {
   return (
     <main>
       <nav className="m-4 flex flex-row items-center gap-4 text-gray-400 font-mono font-medium">
-        <div className="flex flex-row items-center select-none">
-          <h1 className="text-3xl text-violet-400 -skew-y-12">EZ</h1>
-          <h1 className="text-3xl text-emerald-500 -skew-y-12">3D</h1>
-        </div>
+        <Logo />
 
-        <div className="flex flex-col gap-1">
+        <div className="flex flex-col">
           <div>
-            <div className="fill-gray-100 flex flex-row gap-4">
-              <span className="text-sm text-gray-100">a really cool scene</span>
-              <button>
+            <div className="flex flex-row gap-4">
+              <span className="text-gray-200">a really cool scene</span>
+              <button className="fill-gray-200 hover:fill-emerald-500">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 512 512"
-                  height="18px"
                   width="18px"
+                  height="18px"
                 >
-                  <path d="M362.7 19.3L314.3 67.7 444.3 197.7l48.4-48.4c25-25 25-65.5 0-90.5L453.3 19.3c-25-25-65.5-25-90.5 0zm-71 71L58.6 323.5c-10.4 10.4-18 23.3-22.2 37.4L1 481.2C-1.5 489.7 .8 498.8 7 505s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L421.7 220.3 291.7 90.3z" />
+                  <path d="M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1v32c0 8.8 7.2 16 16 16h32zM362.7 18.7L348.3 33.2 325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z" />
                 </svg>
               </button>
             </div>
@@ -203,8 +201,30 @@ function App() {
           <span className="text-[11px]">scotth</span>
         </div>
 
-        <button className="ml-auto text-sm">save</button>
-        <button className="text-sm">profile</button>
+        <div className="ml-auto text-[11px] flex flex-col gap-[10px]">
+          <div className="flex gap-[10px] justify-end">
+            <button className="bg-zinc-900 text-gray-200 hover:text-emerald-500 rounded-[10px] px-4 h-[24px]">
+              save
+            </button>
+            <button className="bg-zinc-900 text-gray-200 hover:text-emerald-500 rounded-[10px] px-4 h-[24px]">
+              export
+            </button>
+            <button className="bg-zinc-900 text-gray-200 hover:text-emerald-500 rounded-[10px] px-4 h-[24px]">
+              profile
+            </button>
+          </div>
+          <div className="flex gap-[10px] justify-end">
+            <button className="bg-zinc-900 text-gray-200 hover:text-emerald-500 rounded-[10px] px-4 h-[24px] shrink-0">
+              + light
+            </button>
+            <button className="bg-zinc-900 text-gray-200 hover:text-emerald-500 rounded-[10px] px-4 h-[24px] shrink-0">
+              + geometry
+            </button>
+            <button className="bg-zinc-900 text-gray-200 hover:text-emerald-500 rounded-[10px] px-4 h-[24px] shrink-0">
+              show hints
+            </button>
+          </div>
+        </div>
       </nav>
       <div
         id="canvas-container"
---
+++ src/components/Logo.jsx
@@ -0,0 +1,15 @@
+const Logo = () => (
+  <div className="relative flex flex-row items-center select-none">
+    <div class="absolute w-[54px] h-[54px] left-1/2 transform -translate-x-1/2 skew-y-6 rounded-[6px] p-[2px] bg-gradient-to-br from-blue-400 via-cyan-400 to-emerald-500">
+      <div class="flex h-full w-full bg-transparent bg-zinc-800 rounded-[4px]"></div>
+    </div>
+    <h1 className="text-3xl bg-zinc-800 text-gray-200 -skew-y-12 [text-shadow:_2px_4px_3px_rgb(0_0_0_/_40%)]">
+      EZ
+    </h1>
+    <h1 className="text-3xl bg-zinc-800 text-gray-200 -skew-y-12 [text-shadow:_2px_4px_3px_rgb(0_0_0_/_40%)]">
+      3D
+    </h1>
+  </div>
+);
+
+export default Logo;
