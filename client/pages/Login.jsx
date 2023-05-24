14a9246d554a8905766146c9cd4aa2a32438e055
Scott Hallock <scott.hallock@gmail.com>
2023-05-24 16:11:31 -0500

    feat: login and signup page

--- client/pages/Login.jsx
+++ client/pages/Login.jsx
@@ -1,5 +1,55 @@
+import { Link } from 'react-router-dom';
+import Logo from '../components/Logo';
+
 const Login = () => {
-  return <h1>This is the login page</h1>;
+  return (
+    <main className="m-4 flex flex-col gap-4">
+      <div className="flex gap-4 items-center">
+        <Link to="/">
+          <Logo />
+        </Link>
+        <Link to="/signup" className="ml-auto">
+          <div className="bg-zinc-900 shadow-900 rounded-[10px] p-3 hover:text-emerald-500">
+            Sign Up
+          </div>
+        </Link>
+      </div>
+
+      <div className="flex flex-col items-center">
+        <div className="rounded-[12px] p-[2px] bg-gradient-to-br from-blue-400 via-cyan-400 to-emerald-500">
+          <div className="flex flex-col gap-2 bg-zinc-900 p-10 rounded-[10px]">
+            <span className="text-center mb-4">Log In</span>
+            <label htmlFor="username" className="text-gray-400">
+              Username or email
+            </label>
+            <input
+              name="username"
+              type="text"
+              className="bg-gray-700 px-2 py-1 rounded-[3px] mb-4"
+            />
+
+            <label htmlFor="password" className="text-gray-400">
+              Password
+            </label>
+            <input
+              name="password"
+              type="password"
+              className="bg-gray-700 px-2 py-1 rounded-[3px] mb-4"
+            />
+
+            <div className="flex justify-center">
+              <button
+                className="hover:text-emerald-500"
+                onClick={() => console.log('clicked submti')}
+              >
+                Submit
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default Login;
--- client/pages/Signup.jsx
+++ client/pages/Signup.jsx
@@ -1,20 +1,60 @@
-const Login = () => {
+import { Link } from 'react-router-dom';
+import Logo from '../components/Logo';
+
+const Signup = () => {
   return (
-    <main>
-      <main className="m-4 flex flex-col gap-4">
-        <div className="flex">
+    <main className="m-4 flex flex-col gap-4">
+      <div className="flex gap-4 items-center">
+        <Link to="/">
           <Logo />
-          <Link to="/signup">View showcase</Link>
-          <Link to="/login">Login or sign up</Link>
+        </Link>
+        <Link to="/login" className="ml-auto">
+          <div className="bg-zinc-900 shadow-900 rounded-[10px] p-3 hover:text-emerald-500">
+            Log In
+          </div>
+        </Link>
+      </div>
+
+      <div className="flex flex-col items-center">
+        <div className="rounded-[12px] p-[2px] bg-gradient-to-br from-blue-400 via-cyan-400 to-emerald-500">
+          <div className="flex flex-col gap-2 bg-zinc-900 p-10 rounded-[10px]">
+            <span className="text-center mb-4">Create an account</span>
+            <label htmlFor="username" className="text-gray-400">
+              Choose a username
+            </label>
+            <input
+              name="username"
+              type="text"
+              className="bg-gray-700 px-2 py-1 rounded-[3px] mb-4"
+            />
+
+            <label htmlFor="email" className="text-gray-400">
+              Email
+            </label>
+            <input name="email" type="email" className="bg-gray-700 px-2 py-1 rounded-[3px] mb-4" />
+
+            <label htmlFor="password" className="text-gray-400">
+              Choose password
+            </label>
+            <input
+              name="password"
+              type="password"
+              className="bg-gray-700 px-2 py-1 rounded-[3px] mb-4"
+            />
+
+            <div className="flex justify-center">
+              <button
+                className="hover:text-emerald-500"
+                onClick={() => console.log('clicked submti')}
+              >
+                Submit
+              </button>
+            </div>
+          </div>
         </div>
-        <h1 className="text-3xl">Create 3D scenes with ease</h1>
-        <h1 className="text-3xl">Save scenes</h1>
-        <Link to="/scene">Start creating</Link>
-        <Link to="/scene">View showcase</Link>
-        <Link to="/login">Login or sign up</Link>
-      </main>
+      </div>
     </main>
   );
 };
 
-export default Login;
+export default Signup;
